e72acffe92fd5f4191a533a7c7119fef8a74e1d2
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-02 10:31:31 -0400

    initial commit with cloudflare worker

--- app/root.tsx
+++ app/root.tsx
@@ -8,12 +8,6 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import styles from "./styles/app.css";
-
-export function links() {
-  return [{ rel: "stylesheet", href: styles }];
-}
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
