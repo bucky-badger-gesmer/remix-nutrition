1f625f93d1b9a47a7a760109bdb185149d0b5221
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-02 12:32:02 -0400

    some changes to index route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,16 @@
+import Nav from "~/components/Nav";
+
 export default function Index() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Nav />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-red-200 h-screen">
+
+      </div>
+      <div className="bg-blue-200 h-screen">
+
+      </div>
+    </div>
+  );
 }
