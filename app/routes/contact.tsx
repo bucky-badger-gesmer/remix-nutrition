28fb825166cbac3f8483881853f4347e55de7ea7
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-02 12:31:46 -0400

    add contact route

---
+++ app/routes/contact.tsx
@@ -0,0 +1,10 @@
+import Nav from "~/components/Nav";
+
+export default function Contact() {
+  return (
+    <div>
+      <Nav />
+      <h1 className="text-3xl font-bold underline">Contact Me</h1>
+    </div>
+  );
+}
