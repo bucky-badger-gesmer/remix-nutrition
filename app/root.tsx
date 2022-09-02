88099f82e45d0ba22781438dbf7950e1b5232a25
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-02 11:30:31 -0400

    added tailwind to project

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,20 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+// THIS POINTS TO GENERATED APP.CSS:
+import styles from "./styles/app.css";
+
+export function links() {
+  return [
+    { rel: "stylesheet", href: styles },
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+      type: "image/png",
+    },
+  ];
+}
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
