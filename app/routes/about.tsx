707206fabe44681c5fe26aca2f8efc41ac3a4cbe
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-02 12:31:39 -0400

    add about route

---
+++ app/routes/about.tsx
@@ -0,0 +1,10 @@
+import Nav from "~/components/Nav";
+
+export default function About() {
+  return (
+    <div>
+      <Nav />
+      <h1 className="text-3xl font-bold underline">About Me</h1>
+    </div>
+  );
+}
