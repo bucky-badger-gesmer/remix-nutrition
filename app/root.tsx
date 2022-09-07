14f138449493cb0f57258271d8c6fcba0a0480b9
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-07 13:21:56 -0500

    change meta title

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Premier Plate Fitness",
   viewport: "width=device-width,initial-scale=1",
 });
 
