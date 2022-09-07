7d4bc0bd800be62e3fa9e3ca249656ef44975d01
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-07 13:22:45 -0500

    some changes to Nav styling

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -12,41 +12,44 @@ const Nav = () => {
     <nav className="sticky top-0 bg-white flex justify-between items-center h-20 md:h-24 px-8 shadow">
       <a href="index">
         <h1 className="w-full md:text-3xl text-xl font-bold">
-          <span className="hover:cursor-pointer">
-            &#127822; Aaron Gesmer Nutrition
+          <span className="text-red hover:cursor-pointer">
+            &#127869; Premier Plate Fitness
           </span>
         </h1>
       </a>
 
-      <ul className="hidden md:flex">
-        <li className="p-4 hover:cursor-pointer hover:text-red-500">
+      <ul className="hidden md:flex text-gray">
+        <li className="p-4 hover:cursor-pointer hover:text-red">
           <a href="index">Home</a>
         </li>
-        <li className="p-4 hover:cursor-pointer hover:text-red-500">
+        <li className="p-4 hover:cursor-pointer hover:text-red">
           <a href="about">About</a>
         </li>
-        <li className="p-4 hover:cursor-pointer hover:text-red-500">
+        <li className="p-4 hover:cursor-pointer hover:text-red">
           <a href="contact">Contact</a>
         </li>
       </ul>
-      <div className="hover:cursor-pointer hover:text-red-500 block md:hidden" onClick={toggleNav}>
-        {isOpen ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div
+        className="hover:cursor-pointer block md:hidden text-red"
+        onClick={toggleNav}
+      >
+        {isOpen ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
       <div
         className={
           isOpen
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-600 bg-white ease-in-out duration-500"
-            : "fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-600 bg-white ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray bg-white ease-in-out duration-500"
+            : "fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray bg-white ease-in-out duration-500"
         }
       >
-        <ul className="pt-12 uppercase">
-          <li className="p-4 border-b border-gray-600 hover:cursor-pointer hover:text-red-500">
+        <ul className="pt-12 uppercase text-gray">
+          <li className="p-4 border-b border-gray hover:cursor-pointer hover:text-red">
             <a href="index">Home</a>
           </li>
-          <li className="p-4 border-b border-gray-600 hover:cursor-pointer hover:text-red-500">
+          <li className="p-4 border-b border-gray hover:cursor-pointer hover:text-red">
             <a href="about">About</a>
           </li>
-          <li className="p-4 border-b border-gray-600 hover:cursor-pointer hover:text-red-500">
+          <li className="p-4 border-b border-gray hover:cursor-pointer hover:text-red">
             <a href="contact">Contact</a>
           </li>
         </ul>
