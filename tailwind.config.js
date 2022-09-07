bdfe82dacd056c1e4415542a62a01e6b77dc3656
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-07 09:57:12 -0500

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
     fontFamily: {
       sans: ['"Poppins"']
     },
+    colors: {
+      'white': '#EAE7DC',
+      'brown': '#D8C3A5',
+      'gray': '#8E8D8A',
+      'pink': '#E98074',
+      'red': '#E85A4F',
+      // 'blue': '#5680E9',
+      // 'baby-blue': '#84CEEB',
+      // 'sky-blue': '#5AB9EA',
+      // 'violet': '#C1C8E4',
+      // 'purple': '#8860D0',
+    },
     extend: {},
   },
   plugins: [],
