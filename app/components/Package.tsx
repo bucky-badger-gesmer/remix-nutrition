37326fc4777438524c983c657e5f6113d571a8a8
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-13 16:52:00 -0500

    changes to Package props

--- app/components/Package.tsx
+++ app/components/Package.tsx
@@ -7,22 +7,26 @@ interface PackageProps {
   subHeader: string;
 }
 
-const Package = ({ imageSrc, imageAlt, price }: PackageProps) => {
+const Package = ({ imageSrc, imageAlt, price, header, subHeader }: PackageProps) => {
   return (
     <div className="flex flex-col items-center bg-white rounded-lg shadow-md md:flex-row md:max-w-xl">
       <img
-        className="object-cover w-full h-1/2  rounded-t-lg md:h-full md:w-48 md:rounded-none md:rounded-l-lg"
+        className="object-cover w-full h-1/4 md:h-full rounded-t-lg md:w-1/2 md:rounded-none md:rounded-l-lg"
         src={imageSrc}
         alt={imageAlt}
       />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="mb-2 text-2xl font-bold tracking-tight">
-          Noteworthy technology acquisitions 2021
+          {header}
         </h5>
-        <p className="mb-3 font-normal">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+        <p className="font-normal text-gray">
+          {subHeader}
         </p>
+        <a href="services">
+          <button className="content-center bg-red text-white font-bold py-2 px-4 rounded uppercase text-xl md:text-2xl shadow">
+            {`$${price}/monthly`}
+          </button>
+        </a>
       </div>
     </div>
   );
